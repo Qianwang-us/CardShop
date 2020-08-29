419b2664df3e002a0dd61be5b9324aea6ae5bfde
Qianwang-us <qianwang.us@gmail.com>
2020-08-28 20:26:54 -0400

    update sort by dropdown list

--- WebContent/js/index.js
+++ WebContent/js/index.js
@@ -1,23 +1,12 @@
-let selectElement = document.getElementById("display-style");
+
+// commuicate with server to update product display when sort type changes
+let selectElement = document.getElementById("sort-type");
 
 selectElement.addEventListener('change', (event) => {
-
-  if(event.target.value == 'light'){
-    document.body.style.backgroundColor = 'white';
-    document.querySelector(".menu-item-logo h1 a").style.color="black";
-    let menus = document.querySelectorAll("nav.menu-item ul li a");
-    menus[0].style.color="#222";
-    menus[1].style.color="#222";
-  }else if(event.target.value == 'dark'){
-    document.body.style.backgroundColor = 'darkslategray';
-    //document.body.style.color = "white";
-    document.querySelector(".menu-item-logo h1 a").style.color="white";
-    let menus = document.querySelectorAll("nav.menu-item ul li a");
-    menus[0].style.color="white";
-    menus[1].style.color="white";
-  }
+  console.log(event.target.value);
 });
 
+// toggle heart to add to favorite list
 let hearts = document.querySelectorAll(".favoriate i");
 for (let i=0; i<hearts.length; i++){
   hearts[i].addEventListener("click", function(event){
@@ -25,6 +14,6 @@ for (let i=0; i<hearts.length; i++){
     console.log(event.target);
     event.target.classList.toggle("far");
     event.target.classList.toggle("fas");
-    
+
   });
 }
